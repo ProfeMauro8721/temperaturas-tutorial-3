546d9324ffa3dd3e40ca8b884c07c1388df2ec61
ProfeMauro8721 <92501098+ProfeMauro8721@users.noreply.github.com>
2023-07-19 09:26:09 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,20 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     Temperatura = input.temperature()
     Temperatura += -3
+    basic.showString("Temperatura")
     basic.showNumber(Temperatura)
+    basic.showString("Cº")
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
-        servos.P0.setAngle(0)
+        servos.P1.setAngle(0)
         basic.pause(500)
-        servos.P0.setAngle(90)
+        servos.P1.setAngle(90)
         basic.pause(500)
     }
 })
 let Temperatura = 0
-servos.P0.setAngle(0)
+servos.P1.setAngle(0)
 basic.showString("Hola")
 music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 basic.forever(function () {
